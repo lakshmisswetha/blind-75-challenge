c176963114c0e26c64c678899f73c3d7027d68da
lakshmi <lakshmisswetha@gmail.com>
2025-03-21 21:08:49 +0530

    added soln to get_sum

---
+++ Challenge-5/get_sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+
+        while(b>0){
+            b--;
+            a++;
+        }
+        while(b<0){
+            b++;
+            a--;
+        }
+        return a;
+        
+    }
+};
