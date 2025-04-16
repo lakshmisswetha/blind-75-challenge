5581c069a0faeb0e4d60faa846c7f0224dc0d9f6
lakshmi <lakshmisswetha@gmail.com>
2025-04-16 23:00:02 +0530

    added soln to number of islands

---
+++ Challenge-31/no_of_islands.cpp
@@ -0,0 +1,52 @@
+class Solution {
+
+
+private:
+    void bfs(vector<vector<char>>& grid , vector<vector<int>>& vis, int row, int col){
+        vis[row][col] = 1;
+        queue<pair<int, int>> q;
+        q.push({row,col});
+        int n = grid.size();
+        int m = grid[0].size(); 
+
+
+        int drow[] = {-1, 1, 0, 0};
+        int dcol[] = {0, 0, -1, 1};
+
+        while (!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+
+
+            for (int i = 0; i < 4; i++){
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && 
+                    !vis[nrow][ncol] && grid[nrow][ncol] == '1'){
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
+                }
+            }
+        }
+    }
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        int cnt = 0;
+        for (int row = 0; row<n; row++){
+            for(int col =0; col<m; col++){
+                if (grid[row][col]=='1' && !vis[row][col]){
+                    cnt ++;
+                    bfs(grid,vis,row,col);
+                }
+            }
+        }
+        return cnt;
+    }
+};
