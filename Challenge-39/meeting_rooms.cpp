7375095a9bc4d7c41c65480d43ef1a095dff5797
lakshmi <lakshmisswetha@gmail.com>
2025-04-24 12:56:16 +0530

    added soln to meeting rooms

---
+++ Challenge-39/meeting_rooms.cpp
@@ -0,0 +1,19 @@
+bool compareIntervals(const Interval& a, const Interval& b) {
+    return a.start < b.start;
+}
+
+class Solution {
+
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), compareIntervals);
+        
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i].start < intervals[i - 1].end) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
