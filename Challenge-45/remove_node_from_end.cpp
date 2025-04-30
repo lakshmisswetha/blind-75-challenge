18cf877772c9e420c9748b59c6a51ebfd2ed8010
lakshmi <lakshmisswetha@gmail.com>
2025-05-01 00:39:39 +0530

    added soln to remove node from end

---
+++ Challenge-45/remove_node_from_end.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* first = dummy;
+        ListNode* second = dummy;
+
+       
+        for (int i = 0; i <= n; ++i) {
+            first = first->next;
+        }
+
+        
+        while (first != nullptr) {
+            first = first->next;
+            second = second->next;
+        }
+
+        
+        ListNode* nodeToDelete = second->next;
+        second->next = second->next->next;
+        delete nodeToDelete;
+
+        
+        ListNode* newHead = dummy->next;
+        delete dummy; 
+        return newHead;
+    }
+};
